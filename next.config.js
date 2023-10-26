fadd36ce5a7c79ea46bc5d791b8da589ebe0a755
pdeprizio <paigedeprizio@gmail.com>
2023-10-25 22:16:44 -0400

    added bug fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export', // Enable static HTML export
     experimental: {
       images: true,
+      // other experimental options...
     },
     // other configurations...
   };
   
   module.exports = nextConfig;
-
-
+  
