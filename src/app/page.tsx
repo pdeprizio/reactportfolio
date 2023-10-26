d390d0ecb10a7efe4a962c89f9e1be12fe1a6b89
pdeprizio <paigedeprizio@gmail.com>
2023-10-25 21:01:04 -0400

    fixed errors! up and running xx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import { useState } from 'react';
 import {BsFillBrushFill} from 'react-icons/bs';
 import {BsGraphUpArrow} from 'react-icons/bs';
 import {BsFillLightbulbFill} from 'react-icons/bs';
+import 'tailwindcss/tailwind.css'; 
 
 
 
