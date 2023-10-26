5c5a84a6f4ca478d5dd3d78057879bc163f6376f
pdeprizio <paigedeprizio@gmail.com>
2023-10-26 09:52:11 -0400

    added another fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '' : ''; // For production, basePath is empty
+const assetPrefix = isProduction ? '/' : '/'; // For production, assetPrefix is '/'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/reactportfolio', // Specify the base path
-    assetPrefix: '/reactportfolio/', // Specify the asset prefix
-  
-    output: 'export', // Enable static HTML export
-    experimental: {
-      images: true,
-      // other experimental options...
-    },
-    // other configurations...
-  };
-  
-  module.exports = nextConfig;
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  output: 'export', // Enable static HTML export
+  experimental: {
+    images: true,
+    // other experimental options...
+  },
+  // other configurations...
+};
+
+module.exports = nextConfig;
