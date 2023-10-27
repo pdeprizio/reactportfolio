03ae164c8a1d39d800d4f8bd27ad42136ced30f3
pdeprizio <paigedeprizio@gmail.com>
2023-10-26 23:20:43 -0400

    bug

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 const isProduction = process.env.NODE_ENV === 'production';
-const basePath = isProduction ? '' : ''; // For production, basePath is empty
-const assetPrefix = isProduction ? '/' : '/'; // For production, assetPrefix is '/'
+const basePath = isProduction ? '' : '';
+const assetPrefix = isProduction ? '/' : '/';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: basePath,
   assetPrefix: assetPrefix,
   // Enable static HTML export
-  experimental: {
-    images: true,
-    // other experimental options...
-  },
+
   // other configurations...
 };
 
 module.exports = nextConfig;
+
